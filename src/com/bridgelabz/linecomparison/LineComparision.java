4bdad9019617c6419649277e5aabec149b6c2c1e
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-01-19 17:23:37 +0530

    refactor : compare two lines using oops concepts

--- src/com/bridgelabz/linecomparison/LineComparision.java
+++ src/com/bridgelabz/linecomparison/LineComparision.java
@@ -3,46 +3,48 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class LineComparision{
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
-       
-
+    static double length1, length2, length;
+    static int x1, x2, y1, y2;
+    public static void coordinate(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter point x1: ");
-        int x1 = sc.nextInt();
-        System.out.println("Enter point y1: ");
-        int y1 = sc.nextInt();
-        System.out.println("Enter point x2: ");
-        int x2 = sc.nextInt();
-        System.out.println("Enter point y2: ");
-        int y2 = sc.nextInt();
-        double length1 = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
-        System.out.println("Length of first line: " + length1);
-        System.out.println("\nEnter points for second line:");
-        System.out.println("Enter point x3: ");
-        int x3 = sc.nextInt();
-        System.out.println("Enter point y3: ");
-        int y3 = sc.nextInt();
-        System.out.println("Enter point x4: ");
-        int x4 = sc.nextInt();
-        System.out.println("Enter point y4: ");
-        int y4 = sc.nextInt();
-        Double length2 = Math.sqrt((Math.pow((x4 - x3), 2)) + (Math.pow((y4 - y3), 2)));
-        System.out.println("Length of second line is: " + length2);
-        int result = length2.compareTo(length1);
-
-        if (result == 0) {
-            System.out.println("\nLines are equal");
-        } else if (result > 0) {
-            System.out.println("\nLine 2 is greater than line 1");
-        } else {
-            System.out.println("\nline 1 is greater than line 2");
-        }
+        System.out.println("Enter Values for x1, x2, y1, y2 : ");
         
-        sc.close();
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int y2 = sc.nextInt();
 
-    }
+        length = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2)); // lenths
+
+        }
+        public static void calLength(){
+            System.out.println("Enter Value For First Line");
+            coordinate();
+            length1 = length;
+            System.out.println("Enter Value For Second Line");
+            coordinate();
+            length2 = length;
+            System.out.println("Length of Line1 : "+ length1);
+             System.out.println("Length of Line2 : "+ length2);
+        }
+
+        public static void compareLines(){
+            if(length1 == length2){
+                System.out.print("Both Lines are equal");
+            }
+            else if(length1>length2){
+                System.out.print("Line1 is greater than Line2");
+            }
+            else{
+                System.out.print("Line2 is greater than Line1");
+            }
+        }
+
+        public static void main(String args[]){
+            calLength();
+            compareLines();
+        }
+
 }
 
     
